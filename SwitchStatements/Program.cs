e8378c766276f62460813272ae373862daa8a0bd
Matthew Bonner <matthewsbonner@comcast.net>
2021-11-19 10:32:47 -0500

    commit 2

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,31 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your favorite school subject? Don't forget to capitalize the first letter");
+
+            var answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "Math":
+                    Console.WriteLine("Math is my favorite as well!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is cool.");
+                    break;
+                case "History":
+                    Console.WriteLine("History is important.");
+                    break;
+                case "English":
+                    Console.WriteLine("English is tough...");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("Muy bien, yo también.");
+                    break;
+                default:
+                    Console.WriteLine("I'm not familiar, but that sounds intriguing.");
+                    break;
+            }
         }
     }
 }
