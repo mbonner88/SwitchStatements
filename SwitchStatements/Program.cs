52a625ba6da3e3d8bbc0141a6de14b796b612b35
Matthew Bonner <matthewsbonner@comcast.net>
2021-11-19 10:04:02 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
